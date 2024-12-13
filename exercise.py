e06a52955f58bd31abdc0a861908b513e2d359f6
awesomepants459 <jonnyspansail@gmail.com>
2024-12-13 15:17:24 -0600

    exercise

---
+++ exercise.py
@@ -0,0 +1,17 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('books.db')
+curs = conn.cursor()
+
+pd.read_sql('SELECT last FROM authors ORDER BY last DESC', conn)
+
+pd.read_sql('SELECT title FROM titles ORDER BY title ASC', conn)
+
+pd.read_sql('SELECT ')
+
+curs = curs.execute("""INSERT INTO authors (first, last) VALUES ('Jon', 'Spansail')""")
+
+#adding new title for author into isbn table and titles
+curs = curs.execute("""INSERT INTO author_ISBN (isbn) VALUES ('3128987601')""")
+curs = curs.execute("""INSERT INTO tables (isbn, title, edition, copyright) VALUES ('3128987601', 'Renegade: The Jason Dent Story', '1', '2024 Bloodmoon LTSS')""")
